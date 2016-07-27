32d4f22d5b96addedb931de09696f72f309f73f6
ayezee33 <ayezee33@gmail.com>
2016-07-26 18:35:07 -0700

    add font awesome icons and conditional checks for imdb rating. Also added functionality for submitting the form with enter button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 $(document).foundation();
 
-var movieObject = [];
 $('#movieSubmit').on('click', function(e) {
   e.preventDefault();
+  getMovie();
+});
+
+$("input").keypress(function(e) {
+  if (e.which == 13) {
+      e.preventDefault();
+      $("#movieSubmit").submit();
+      getMovie();
+  }
+});
+
+function getMovie () {
   var movie  = $('#movie').val();
   var newMovie = movie.replace(/\s+/g, '');
   var requestUrl = 'http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json'
@@ -16,14 +27,22 @@ $('#movieSubmit').on('click', function(e) {
     var plot       = data.Plot
     var imdbRating = data.imdbRating
     console.log(data);
+    if (imdbRating < 6 ) {
+      ratingLabel = 'alert'
+      ratingIcon = '<i class="fa fa-thumbs-down" aria-hidden="true"></i>'
+    } else {
+      ratingLabel = 'success'
+      ratingIcon = '<i class="fa fa-thumbs-up" aria-hidden="true"></i>'
+    }
+
     $('#output').html('<div class="callout large">' +
       '<img class="float-left" src="' + poster + '">' +
       '<h3>' + title + '</h3>' +
-      '<span class="success label">Rating: ' + rated + '</span>' +
+      '<span class="sucess label">Rating: ' + rated + '</span>' +
       '<p>Released Date: ' + released + '</p>' +
       '<p>Actors: ' + actors + '</p>' +
-      '<p>IMDb rating: ' + imdbRating + '</p>' +
+      '<p>IMDb rating: <span class="' + ratingLabel + ' label">' + imdbRating + ratingIcon + '</span></p>' +
       '<p class="plot">Summary: ' + plot + '</p>' +
       '</div>');
     });
-});
+  }
